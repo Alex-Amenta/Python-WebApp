81bda80abd1d05fdd689bfd668c6d7b1c9010929
Alexcarp-ar <alexriverplate06@gmail.com>
2023-08-30 13:58:14 -0300

    deployy

--- src/index.py
+++ src/index.py
@@ -1,6 +1,22 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Obtén la ruta completa al directorio actual (donde se encuentra index.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construye la ruta completa al archivo requirements.txt dentro de la carpeta src
+requirements_file = os.path.join(current_dir, 'src', 'requirements.txt')
+
+# Lee las dependencias desde el archivo requirements.txt
+with open(requirements_file) as f:
+    dependencies = f.read().splitlines()
+
+# Instala las dependencias
+for dependency in dependencies:
+    if dependency:
+        os.system(f'pip install {dependency}')
 
 @app.route('/')
 def home():
